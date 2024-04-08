997943554edba61c366ac2582d7dd1e7574f4b2f
dintory <84258988+dintory@users.noreply.github.com>
2024-04-08 18:20:48 -0400

    Create keybind.lua

---
+++ Modules/keybind.lua
@@ -0,0 +1,42 @@
+local Keybind = {
+    Functions = {},
+    Assigned = {}
+}
+
+local UserInputService = game:GetService("UserInputService")
+
+local Thread, NewInputConnection = task.spawn, UserInputService.InputBegan
+
+function Keybind:SetUpKey(Bind, Name, Action)
+    table.insert(Keybind.Assigned, {
+        Name = Name,
+        Keybind = Bind,
+        Action = Action
+    })
+end
+
+function Keybind:Functions(Input)
+    for i,v in pairs(Keybind.Assigned) do
+        if v.Keybind == Input.KeyCode then
+            return v.Action()
+        end
+    end
+end
+
+function Keybind:ChangeKey(Name, New_Key)
+    for i,v in ipairs(Keybind.Assigned) do
+        if v.Name == Name then
+            v.Keybind = New_Key
+        end
+    end
+end
+
+Thread(function() -->> putting it in a thread
+    NewInputConnection:Connect(function(input, gameProcessedEvent)
+        if gameProcessedEvent then return end
+        Keybind:Functions(input)
+    end)
+end)
+
+
+return Keybind
